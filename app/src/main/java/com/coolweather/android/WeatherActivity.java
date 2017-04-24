0eafd7aafca1a60ae6ea8b112028cf726f35ebc8
Gread <gread@gmail.com>
2017-04-24 17:18:01 +0800

    first commit test

--- app/src/main/java/com/coolweather/android/WeatherActivity.java
+++ app/src/main/java/com/coolweather/android/WeatherActivity.java
@@ -130,6 +130,7 @@ public class WeatherActivity extends AppCompatActivity {
      * 根据天气id请求城市天气信息。
      */
     public void requestWeather(final String weatherId) {
+        Toast.makeText(WeatherActivity.this, "This is my fork edition", Toast.LENGTH_SHORT).show();
         String weatherUrl = "http://guolin.tech/api/weather?cityid=" + weatherId + "&key=bc0418b57b2d4918819d3974ac1285d9";
         HttpUtil.sendOkHttpRequest(weatherUrl, new Callback() {
             @Override
